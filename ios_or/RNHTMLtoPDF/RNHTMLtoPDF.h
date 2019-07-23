576dbe549dbf7dd50aa35e0b2f26af23869304c6
Cassio Seffrin <cassioseffrin@gmail.com>
2019-07-23 15:12:44 -0300

    ignored files

--- ios_or/RNHTMLtoPDF/RNHTMLtoPDF.h
+++
@@ -1,9 +0,0 @@
-
-//  Created by Christopher on 9/3/15.
-
-#import <React/RCTView.h>
-#import <React/RCTBridgeModule.h>
-
-@interface RNHTMLtoPDF : RCTView <RCTBridgeModule, UIWebViewDelegate>
-
-@end
